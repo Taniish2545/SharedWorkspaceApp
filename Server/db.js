009958656b09a447d6e7b01873a79378a5b59840
Vrajmistry0310 <vrajmistry77@gmail.com>
2025-08-22 12:22:26 -0600

    Update db.js

--- Server/db.js
+++ Server/db.js
@@ -1,1 +1,12 @@
+// Server/db.js
+import mongoose from 'mongoose';
 
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+  } catch (err) {
+    throw err;
+  }
+};
+
+export default connectDB;
