fdab014aaf3f07a14436dddf4d0b34ab71899e54
het patel <het6413@gmail.com>
2025-08-23 00:15:33 -0600

    Update property.js

--- Server/models/property.js
+++ Server/models/property.js
@@ -12,3 +12,17 @@ const propertySchema = new mongoose.Schema({
 }, { timestamps: true });
 
 export default mongoose.model('Property', propertySchema);
+// Server/models/Property.js
+import mongoose from 'mongoose';
+
+const propertySchema = new mongoose.Schema({
+  address:      { type: String, trim: true, required: true },
+  neighborhood: { type: String, trim: true, default: '' },
+  sqft:         { type: Number, default: 0 },
+  parking:      { type: Boolean, default: false },
+  transit:      { type: Boolean, default: false },
+  photos:       [{ type: String }],
+  owner:        { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+}, { timestamps: true });
+
+export default mongoose.model('Property', propertySchema);
